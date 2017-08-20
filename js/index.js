698ebe0e3884b9663dbea4b2d8dda6704ccf9774
Nechai <vetalya444@mail.ru>
2017-08-20 21:34:20 +0300

    Working web-page

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	
+	$(".text-box").animated("fadeInUp", "fadeOutUp");
+	$(".initials").animated("fadeInUp", "fadeOutUp");
+	$(".section_header").animated("fadeInUp", "fadeOutDown");
+	$(".animation_1").animated("flipInY", "fadeOutDown");
+	$(".animation_2").animated("fadeInLeft", "fadeOutDown");
+	$(".personal-info").animated("fadeInRight", "fadeOutDown");
+	$(".left").animated("fadeInRight", "fadeOutRight");
+	$(".right").animated("fadeInLeft", "fadeOutLeft");
+
+
+
+});
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+	};
